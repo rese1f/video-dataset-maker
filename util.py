810992d1a7bb01b694d6d0a2ba9af131d493a266
Reself <846074811@qq.com>
2022-07-18 06:41:08 -0500

    json helper function

--- util.py
+++ util.py
@@ -1,4 +1,33 @@
+import json
+import re
+import uuid
 
+
+class NoIndent(object):
+    def __init__(self, value):
+        self.value = value
+
+# json custom indent
+class NoIndentEncoder(json.JSONEncoder):
+    def __init__(self, *args, **kwargs):
+        super(NoIndentEncoder, self).__init__(*args, **kwargs)
+        self.kwargs = dict(kwargs)
+        del self.kwargs['indent']
+        self._replacement_map = {}
+
+    def default(self, o):
+        if isinstance(o, NoIndent):
+            key = uuid.uuid4().hex
+            self._replacement_map[key] = json.dumps(o.value, **self.kwargs)
+            return "@@%s@@" % (key,)
+        else:
+            return super(NoIndentEncoder, self).default(o)
+
+    def encode(self, o):
+        result = super(NoIndentEncoder, self).encode(o)
+        for k, v in iter(self._replacement_map.items()):
+            result = result.replace('"@@%s@@"' % (k,), v)
+        return result
 
 def download_resolution(h):
     for i in [144, 360, 480, 720, 1080, 1440, 2160]:
